ec792c44ba369f86fe93777743ae7bd4d19f39ce
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-08-31 19:58:30 +0530

    Jacoco added

--- src/main/java/ValidateAge.java
+++ src/main/java/ValidateAge.java
@@ -1,7 +1,7 @@
 public class ValidateAge {
 
     public int validateGivenAge(int age) {
-        if (age > 18) {
+        if (age >= 18) {
             return 1;
         } else
             return 0;
--- src/test/java/ValidateAgeTest.java
+++ src/test/java/ValidateAgeTest.java
@@ -1,17 +1,16 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidateAgeTest {
 
-//    ValidateAge validateAge = new ValidateAge();
-    private ValidateAge validateAge;
-
-    @BeforeAll
-    void initiateClass(){
-        validateAge=new ValidateAge();
-    }
+    ValidateAge validateAge = new ValidateAge();
+//    private ValidateAge validateAge;
+//
+//    @BeforeAll
+//    void initiateClass(){
+//        validateAge=new ValidateAge();
+//    }
     @Test
     void validAge() {
         assertEquals(1, validateAge.validateGivenAge(20));
